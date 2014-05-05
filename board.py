4609714211ac7af32d3e17d7812f3a797d54ec70
Sam Spekreijse <speksam@gmail.com>
2014-05-05 16:52:52 +1200

    Made not default

--- board.py
+++ board.py
@@ -1,3 +1,9 @@
 '''The implementation of the board class'''
 class Board(object):
-    pass
+    dimx, dimy = 3
+    grid = []
+    for i in range(dimx):
+        temp = []
+        for j in range(dimy):
+            temp.append(' ')
+        grid.append(temp)
