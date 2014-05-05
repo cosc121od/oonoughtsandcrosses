462ed5753800e6c4746653f4c03ac6a101aa75ca
Sam Spekreijse <speksam@gmail.com>
2014-05-05 18:17:54 +1200

    Adding Python Built-ins
    Added __init()__ and __str__() methods.

--- board.py
+++ board.py
@@ -1,9 +1,31 @@
 '''The implementation of the board class'''
 class Board(object):
-    dimx, dimy = 3
-    grid = []
-    for i in range(dimx):
-        temp = []
-        for j in range(dimy):
-            temp.append(' ')
-        grid.append(temp)
+
+    def __init__(self):
+        self.dimx = 3
+        self.dimy = 3
+        self.grid = []
+        for i in range(self.dimx):
+            temp = []
+            for j in range(self.dimy):
+                temp.append(' ')
+            self.grid.append(temp)
+    
+    
+    def __str__(self):
+        output = ''
+        for j in range(self.dimy):
+            line = ''
+            for i in range(self.dimx):
+                line += self.grid[i][j]
+                if i != self.dimx-1: line += '|'
+            output += line + '\n'
+            if j != self.dimy-1:
+                line = '-'
+                for i in range(self.dimx - 1):
+                    line += '+-'
+                output += line + '\n'
+        return output
+
+                    
+    
