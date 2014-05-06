5d54367b685e54c9ed9fbb417aad2fc084f79202
CourtsJ <courteneybrownie@gmail.com>
2014-05-07 11:45:18 +1200

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,30 @@
 '''The implementation of the game class'''
-
 class Game(object):
-    pass
+    def __init__(self, player1, player2, current_player, board):
+        self.player1 = player()
+        self.player2 = player()
+        self.current_player = current_player
+        self.board = board()
+        
+    def winner(self):
+        '''Annonces who won the game'''
+        if self.current_player == 1:
+            win = self.player1.name
+        if self.current_player == 2:
+            win = self.player2.name
+        return '{} wins'.format(win)          
+        
+    def is_won(self):
+        '''Sees if game is won'''
+        gameover = self.game_over()
+        if gameover == True:
+            if board.has_winning_line() == True:
+                winner()  
+            else:
+                is_drawn()
+    
+    def is_drawn(self):
+        '''Sees if game has ended in a draw'''    
+        return "Game is drawn"       
+
+        
