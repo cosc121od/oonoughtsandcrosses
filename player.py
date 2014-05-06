bdaa1834c7346e6013f3f61e2b1942911876b4c6
garrattjonathon <garrattjonathon@gmail.com>
2014-05-07 11:21:15 +1200

    Player Class
    contains method get_move which returns a tuple containing row, column

--- player.py
+++ player.py
@@ -2,4 +2,11 @@
 class Player(object):
     
     def __init__(self, name, piece):
+        self.name = name
+        self.piece = piece
         
+    def get_move():
+        row = int(input())
+        column = int(input())
+        x_y = row,column
+        return x_y
