18b4f5872ef62460ebe2f8d7e730b40c0660fb1d
Sam Spekreijse <speksam@gmail.com>
2014-05-07 11:40:25 +1200

    Removing Git tomfoolery

--- board.py
+++ board.py
@@ -1,16 +1,10 @@
 '''The implementation of the board class'''
 class Board(object):
-<<<<<<< HEAD
 
     def __init__(self):
         self.dimx = 3
         self.dimy = 3
-        self.grid = []
-        for i in range(self.dimx):
-            temp = []
-            for j in range(self.dimy):
-                temp.append(' ')
-            self.grid.append(temp)
+        self.grid = [['' for i in range(dimy)] for i in range(dimx)]
     
     
     def __str__(self):
@@ -27,21 +21,6 @@ class Board(object):
                     line += '+-'
                 output += line + '\n'
         return output
-
-                    
-    
-=======
-    dimx, dimy = 3, 3
-    grid = []
-    #first dimention are the rows, second is each of the data values inside each row
-    
-    def __init__(self):
-        'initiates the Board class'
-        for i in range(dimx):
-            temp = []
-            for j in range(dimy):
-                temp.append('')
-            grid.append(temp)
     
     def set_point(self, xytuple, value):
         'sets X or O at a point on the board'
@@ -57,12 +36,11 @@ class Board(object):
         'returns true if the position Xytuple on the grid is free'
         return self.grid[y][x] == ''
     
-    def is_triple(self, xytuple):
-        'returns true if there is a triple intersecting x,y. To be used after set_point'
+    def get_game_state(self):
+        'Does shit get off my back'
         pass
     
     def reset(self):
         'resets the board'
         self.grid = []
         self.__init__()
->>>>>>> 2a84e735db85bdd60d77144f54fcd59d23c75e35
