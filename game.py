5c74b2291d689a3eca76f259f77719b0b2c227e7
asrioth <liamswords@gmail.com>
2014-05-08 16:08:11 +1200

    Update game.py

--- game.py
+++ game.py
@@ -1,42 +1,78 @@
-class game(object):
+class Game(object):
     
     def __init__(self, player1, player2):
-        self.palyer1 = player(player1)
-        self.player2 = player(player2)
+        self.board = Board()
+        self.player1 = player1
+        self.player2 = player2
         self.current_player = 2
-        self.board = board()
         
-    
+        
     def play(self):
-        """runs through the game"""
-        end = self.game_over()
-        while (end == False):
-            current_player = next_player()
-            if current_player == 1:
-                place = self.player1.get_move()
-                self.board.set_token(place, self.player1.piece)
-            elif current_player == 2:
-                place = self.player2.get_move()
-                self.board.set_token(place, self.player2.piece)
-            end = self.game_over()
-                
-        
-    def game_over(self):
-        """tests if the end condition has been met"""
-        if self.board.is_full() == True or self.board.is_winning_line() == True:
-            self.is_won()
+        self.board.display()
+        gameover = self.gameover()
+        while gameover == False:
+            self.next_player()
+            is_empty = False
+            while is_empty == False:
+                if self.current_player == 1:
+                    move = self.player1.get_move()
+                elif self.current_player == 2:
+                    move = self.player2.get_move()
+                is_empty = self.board.is_empty(move)
+                if is_empty == False:
+                    print('There is already a piece in this posistion')
+            if self.current_player == 1:
+                self.board.set_token(move, self.player1.piece)
+            elif self.current_player == 2:
+                self.board.set_token(move, self.player2.piece)
+            gameover = self.gameover()
+            self.board.display()
+        print (self.winner())
+            
+    def gameover(self):
+        is_won = self.is_won()
+        is_draw = self.is_draw()
+        if is_won == True or is_draw == True:
             return True
         else:
             return False
     
     
+    def is_won(self):
+        is_won = False
+        for row in range(3):
+            line = self.board.line(row)
+            if self.is_winning_line(line) == True:
+                is_won = True
+        for col in range(3):
+            col += 3
+            line = self.board.line(col)
+            if self.is_winning_line(line) == True:
+                is_won = True
+        for diagonal in range(2):
+            diagonal += 6
+            line = self.board.line(diagonal)
+            if self.is_winning_line(line) == True:
+                is_won = True
+        return is_won
+    
+    def is_draw(self):
+        is_draw = self.board.is_full()
+        return is_draw
+            
+    def is_winning_line(self, line):
+        is_winning = False
+        if line[0] == line[1] and line[0] == line[2] and line[0] != '-':
+            is_winning = True
+        return is_winning
+
     def next_player(self):
         """switches which players turn it is"""
-        if current_player == 1:
-            return 2
-        elif current_player == 2:
-            return 1
-        
+        if self.current_player == 1:
+            self.current_player = 2
+        elif self.current_player == 2:
+            self.current_player = 1
+    
     
     def winner(self):
         '''Annonces who won the game'''
@@ -44,17 +80,4 @@ class game(object):
             win = self.player1.name
         if self.current_player == 2:
             win = self.player2.name
-        return '{} wins'.format(win)          
-                
-    def is_won(self):
-        '''Sees if game is won'''
-        gameover = self.game_over()
-        if gameover == True:
-            if board.has_winning_line() == True:
-                winner()  
-            else:
-                is_drawn()
-            
-    def is_drawn(self):
-        '''Sees if game has ended in a draw'''    
-        return "Game is drawn"
+        return '{} wins'.format(win)
